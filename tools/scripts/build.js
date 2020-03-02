1d28782657496ca2564df81e8266572638ccfadf
Nivrith <nivrithgomatam@gmail.com>
2020-03-02 21:49:05 +1030

    update build

--- tools/scripts/build.js
+++ tools/scripts/build.js
@@ -9,9 +9,15 @@ const concat = require('concat');
   ];
   await fs.ensureDir('elements');
   await concat(files, 'elements/elements.js');
-  await fs.copyFile(
-    './dist/apps/components/styles.css',
-    'elements/styles.css'
-  );
+
+  const es5files = [
+    './dist/apps/components/runtime-es5.js',
+    './dist/apps/components/polyfills-es5.js',
+    // './dist/apps/components/scripts-es2015.js',
+    './dist/apps/components/main-es5.js'
+  ];
+  await fs.ensureDir('elements');
+  await concat(es5files, 'elements/elements.js');
+  await fs.copyFile('./dist/apps/components/styles.css', 'elements/styles.css');
   // await fs.copy('./dist/apps/components/assets/', 'elements/assets/');
 })();
