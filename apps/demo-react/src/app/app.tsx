26473ed6065d6cfb0a2ed9a599e3598ecf7df031
Lakshya Khera <lakshya.khera@gmail.com>
2020-04-22 15:43:22 +0530

    UNF-68 : React Demo added

--- apps/demo-react/src/app/app.tsx
+++ apps/demo-react/src/app/app.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.css';
 import './app.scss';
-
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
-
+import Container from './components/Container/container.component.tsx'
+import Header from './components/Header/header.component.tsx'
 window['Unfurl']?.setConfig({
   apiToken: 'mdi61be6EhHH8Tdcqt5twdltZmFKRhiF87lWONb7hiF6PODKlHiHOMaQNSMW'
 });
 
 export const App = () => {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./app.scss file.
-   */
   return (
     <div className="app">
-      React
-      <unf-preview url="https://unfurl.io"></unf-preview>
+      <Header/>
+      <Container/>
     </div>
   );
 };
